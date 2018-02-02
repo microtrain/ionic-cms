263c27657c8e7b03126747bd666795104d3b159c
Jason Snider <root@jasonsnider>
2018-02-02 11:49:52 -0600

    Import User into UserCreatePage

--- src/pages/user-create/user-create.ts
+++ src/pages/user-create/user-create.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the UserCreatePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../models/user/user';
 
 @IonicPage()
 @Component({
