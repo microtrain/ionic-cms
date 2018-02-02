a407dfd23fb4e10e71ccfad0d963bd3b874dfb53
Jason Snider <jason@jasonsnider.com>
2018-02-01 21:21:30 -0600

    Removed tutorial comments. Missing import bug fix.

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-//1. Import LoadingController
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { UserProvider } from '../../providers/user/user';
+import { User } from '../../models/user/user';
 
 @IonicPage()
 @Component({
@@ -11,7 +11,6 @@ import { UserProvider } from '../../providers/user/user';
 })
 export class UsersPage {
 
-  //3. Create a space in memory to hold a loader
   private loader: LoadingController;
 
   public users: User[];
@@ -20,7 +19,6 @@ export class UsersPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     private userProvider: UserProvider,
-    //2. Inject the LoadingController
     private loadingCtrl: LoadingController
   ) {
     this.getUsers();
@@ -28,19 +26,16 @@ export class UsersPage {
 
   private getUsers(): void {
 
-    //5. Call the loaded when requesting user data
     this.presentLoader();
 
     this.userProvider.getUsers().subscribe(
       (response)=>{
         this.users = response.users;
-        //6. Dismiss the loader after the HTTP request has completed
         this.loader.dismiss();
       }
     );
   }
 
-  // 4. Create a method and display a loader
   private presentLoader(): void {
 
     this.loader = this.loadingCtrl.create({
