600212a852488d9d6fc3afc86774564ab0004b0e
Jason Snider <jason@jasonsnider.com>
2018-02-01 21:11:44 -0600

    Added a loader

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+//1. Import LoadingController
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 
 import { UserProvider } from '../../providers/user/user';
 
@@ -10,24 +11,43 @@ import { UserProvider } from '../../providers/user/user';
 })
 export class UsersPage {
 
+  //3. Create a space in memory to hold a loader
+  private loader: LoadingController;
+
   public users: User[];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private userProvider: UserProvider
+    private userProvider: UserProvider,
+    //2. Inject the LoadingController
+    private loadingCtrl: LoadingController
   ) {
     this.getUsers();
   }
 
   private getUsers(): void {
+
+    //5. Call the loaded when requesting user data
+    this.presentLoader();
+
     this.userProvider.getUsers().subscribe(
       (response)=>{
-
         this.users = response.users;
-
+        //6. Dismiss the loader after the HTTP request has completed
+        this.loader.dismiss();
       }
     );
+  }
+
+  // 4. Create a method and display a loader
+  private presentLoader(): void {
+
+    this.loader = this.loadingCtrl.create({
+      content: 'Loading...',
+    });
+
+    this.loader.present();
   }
 
 }
